93b10ecd99b5e7526d3d7002ffcb8806997688f0
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-04-23 00:04:51 +0200

    Update plugins and configure blink.cmp completion

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -1,18 +1,33 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
-    version = '0.12.4',
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      cmdline = {
+        enabled = true,
+        completion = {
+          menu = { auto_show = true }
+        },
+      },
       keymap = { preset = 'default' },
 
       appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
 
+      completion = { documentation = { auto_show = false } },
+
       signature = { enabled = true },
+      fuzzy = { implementation = "lua" }
     },
+    opts_extend = { "sources.default" }
   }
 }
