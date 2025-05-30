f0b03dadd1885765b986f273d1b18af15fbcbecb
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-05-30 20:19:53 +0200

    Refactor lsp autocommands

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -17,3 +17,27 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.opt.relativenumber = false
   end,
 })
+
+-- LSP RELATED AUTOCOMMANDS
+vim.api.nvim_create_augroup('LspFormatting', { clear = true })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+
+    if client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = 'LspFormatting',
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+      end
+    end
+  end
+})
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -96,26 +96,6 @@ return {
         filetypes = { 'go' },
         root_dir = require('lspconfig').util.root_pattern('.git', 'go.mod', 'Makefile'),
       }
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
-
-          if client:supports_method('textDocument/formatting', 0) then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
-            if client.server_capabilities.inlayHintProvider then
-              vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-            end
-          end
-        end,
-      })
     end
   }
 }
