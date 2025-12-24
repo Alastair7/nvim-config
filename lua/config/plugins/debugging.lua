c70377f4cf069324c0175b3fc0359a2818c0d145
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-12-24 20:35:38 +0100

    Add debugging to neovim

---
+++ lua/config/plugins/debugging.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+      local vt = require "nvim-dap-virtual-text"
+
+      require("dapui").setup()
+      require("dap-go").setup()
+      require("nvim-dap-virtual-text").setup()
+
+      -- KEYMAP CONFIGURATION
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- evaluate variable under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<leader>dq", function()
+        require("dap").terminate()
+        require("dapui").close()
+        require("nvim-dap-virtual-text").refresh()
+      end)
+      vim.keymap.set("n", "<F13>", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_terminated["vt_cleanup"] = function()
+        vt.refresh()
+      end
+      dap.listeners.before.event_exited["vt_cleanup"] = function()
+        vt.refresh()
+      end
+    end,
+  }
+}
