c3563911d81779fd867f576fee850e0a288659eb
Alastair7 <alexandru.ban.drmer@gmail.com>
2026-07-30 22:45:17 +0200

    Add nvim-treesitter back

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,9 +1,16 @@
+-- return {
+--   {
+--     "romus204/tree-sitter-manager.nvim",
+--     dependencies = {},
+--     config = function()
+--       require("tree-sitter-manager").setup({})
+--     end
+--   }
+-- }
 return {
   {
-    "romus204/tree-sitter-manager.nvim",
-    dependencies = {},
-    config = function()
-      require("tree-sitter-manager").setup({})
-    end
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate'
   }
 }
